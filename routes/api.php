6e595a82aecbbde0ad875cd376293e6ef385f939
FRANKLYN NGAUJAH <you@example.com>
2026-07-23 13:29:03 +0000

    replace current test api file

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Api\ConsultationController;
 
 Route::post('/consultations', [ConsultationController::class, 'store']);
-
-Route::get('/test-mail', function () {
-
-    Mail::raw('LYNPHICS Resend test email', function ($message) {
-        $message->to('franklynngaujah19@gmail.com')
-                ->subject('Resend Test');
-    });
-
-    return response()->json([
-        'message' => 'Test email sent'
-    ]);
-});
