8b9a71ebe48a22571ecb7262265aeaa851959d04
FRANKLYN NGAUJAH <you@example.com>
2026-07-23 13:12:24 +0000

    add test mail tempo

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Api\ConsultationController;
 
 Route::post('/consultations', [ConsultationController::class, 'store']);
+
+Route::get('/test-mail', function () {
+
+    Mail::raw('LYNPHICS Resend test email', function ($message) {
+        $message->to('franklynngaujah19@gmail.com')
+                ->subject('Resend Test');
+    });
+
+    return response()->json([
+        'message' => 'Test email sent'
+    ]);
+});
